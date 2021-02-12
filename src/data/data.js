ae2e3949d2bf88e91f2ed51f854bbd179d384f78
lucia <majlucia@gmail.com>
2021-02-12 15:44:25 +0100

    fixed data

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 const data = [
-    { "index": 1, "url": "https://bandcamp.com/EmbeddedPlayer/album=2127897430/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/3" },
-    { "index": 2, "url": "https://bandcamp.com/EmbeddedPlayer/album=551674705/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/3" },
+    { "index": 1, "url": "https://bandcamp.com/EmbeddedPlayer/album=2127897430/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/1" },
+    { "index": 2, "url": "https://bandcamp.com/EmbeddedPlayer/album=551674705/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/2" },
     { "index": 3, "url": "https://bandcamp.com/EmbeddedPlayer/album=1841959426/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/3" }
 ]
 
