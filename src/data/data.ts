676cd69023982f81e42ad99fcde986aadef5cef4
lucia <majlucia@gmail.com>
2021-02-15 18:15:18 +0100

    added https

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,6 @@
 export const data = [
-    { "index": 1, "name": "Primo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048368&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "http://scienze-desk.eadev.it/audioguida/1" },
-    { "index": 2, "name": "Secondo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048365&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "http://scienze-desk.eadev.it/audioguida/2" },
-    { "index": 3, "name": "Terzo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048359&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "http://scienze-desk.eadev.it/audioguida/3" }
+    { "index": 1, "name": "Primo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048368&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "https://scienze-desk.eadev.it/audioguida/1" },
+    { "index": 2, "name": "Secondo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048365&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "https://scienze-desk.eadev.it/audioguida/2" },
+    { "index": 3, "name": "Terzo audio", "url": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/986048359&color=%230011a1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "external_url": "https://scienze-desk.eadev.it/audioguida/3" }
 ]
 
