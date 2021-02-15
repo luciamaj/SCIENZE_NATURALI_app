36a2eece3906ce7afa095e8d4a30db5ac4208404
lucia <majlucia@gmail.com>
2021-02-15 14:16:25 +0100

    fixed export

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,11 +1,6 @@
-const data = [
+export const data = [
     { "index": 1, "name": "Primo audio", "url": "https://bandcamp.com/EmbeddedPlayer/album=2127897430/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/1" },
     { "index": 2, "name": "Secondo audio", "url": "https://bandcamp.com/EmbeddedPlayer/album=551674705/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/2" },
     { "index": 3, "name": "Terzo audio", "url": "https://bandcamp.com/EmbeddedPlayer/album=1841959426/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true", "external_url": "https://28ztot62o9.appflowapp.com/audioguida/3" }
 ]
-
-
-module.exports = {
-    data
-}
 
