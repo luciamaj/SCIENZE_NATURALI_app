392c8669bc1bad832002b02969fbdd98323bc1d9
lucia <majlucia@gmail.com>
2021-02-22 12:36:25 +0100

    changed path

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,7 +43,7 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'settings',
         name: 'settings',
-        component: () => import('@/views/Amplitude.vue'),
+        component: () => import('@/views/Wave.vue'),
       }
     ]
   }
