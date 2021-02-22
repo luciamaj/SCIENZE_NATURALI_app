d8c424b4878c402c20f18f9a4949fd606a2f6212
lucia <majlucia@gmail.com>
2021-02-22 12:31:06 +0100

    depressione

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/audio/:id',
     name: 'audio',
-    component: () => import('@/views/Audio.vue')
+    component: () => import('@/views/Amplitude.vue')
   },
   {
     path: '/video/:id',
@@ -43,7 +43,7 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'settings',
         name: 'settings',
-        component: () => import('@/views/Wave.vue'),
+        component: () => import('@/views/Amplitude.vue'),
       }
     ]
   }
