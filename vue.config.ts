ba4baccbd4e7076520ec54b828a86cedb55f727b
Arash <arash.rkn@gmail.com>
2022-04-11 11:29:27 +0200

    config app

--- vue.config.ts
+++ vue.config.ts
@@ -2,10 +2,28 @@
 module.exports = {
 
     pwa: {
+        theme_color: '#000',
+        msTileColor: '#000',
 
+        manifestOptions:{
+            name: `'Biblioteca Sonora'`,
+            short_name:`'Biblioteca Sonora'`,
+            start_url: `'.'`,
+            display: `'fullscreen'`,
+            theme_color: `pwa.themeColor`,
+
+        },
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        },
         appleMobileWebAppCapable: 'yes',
 
         appleMobileWebAppStatusBarStyle: 'blue',
+       // workboxPluginMode: 'InjectManifest',
 
         workboxPluginMode: 'GenerateSW',
 
@@ -52,6 +70,7 @@ module.exports = {
                     handler: 'CacheFirst',
 
                 },
+                
                 
 
 
