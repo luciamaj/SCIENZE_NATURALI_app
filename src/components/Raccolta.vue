5e3468c8af36d0e4a8cb076f6ec49a15b2ae668d
Arash <arash.rkn@gmail.com>
2022-05-23 09:55:38 +0200

    raccolta schede già visitate

--- src/components/Raccolta.vue
+++ src/components/Raccolta.vue
@@ -1,70 +1,90 @@
 <template>
-
-<ion-content class="ion-padding">
-    <div class="collection-title">rivivi l'esperienza</div>
-    <div class="list-container">
-         <ion-grid class="schede-grid">
-            <template v-for="scheda in visited" v-bind:key="scheda" >
-                <ion-row v-on:click="openScheda(scheda.tag, scheda.content.type)">
-                    <div class="schede-cont"> 
-                        <ion-col class="scheda-cont-thumb" size="4"  :value="lang"   >
+    <ion-header class="ion-no-border">
+      <ion-toolbar >
+        <ion-title > {{$t('raccolta.title')}}</ion-title>
+       <ion-buttons slot="start" >
+        <ion-button  @click="back"><ion-icon size="large" name="chevron-back" /></ion-button>
+      </ion-buttons>
+      </ion-toolbar> 
+    </ion-header>
+    <ion-content class="ion-padding">
+        <div class="collection-title"> {{$t('raccolta.subtitle')}}</div>
+        <div class="list-container">
+            <ion-grid class="schede-grid" v-if="visited!=null">
+                <template v-for="scheda in visited" v-bind:key="scheda" >
+                    <ion-row v-on:click="openScheda(scheda.tag, scheda.content.type)">
+                        <div class="schede-cont"> 
+                            <ion-col class="scheda-cont-thumb" size="4"  :value="lang"   >
+                            
+                                <div class="thumb-cont"  > <img class="cover thumb" id="thumb-it" :src="this.$store.getters.baseUrl+'/upload/'+scheda.img" alt=""></div>
                         
-                            <div class="thumb-cont"  > <img class="cover thumb" id="thumb-it" :src="this.$store.getters.baseUrl+'/upload/'+scheda.img" alt=""></div>
-                    
-                        </ion-col>
-                        <ion-col class="scheda-cont-name">
-                            <div class="name" >  {{scheda.content.titolo}}</div>
+                            </ion-col>
+                            <ion-col class="scheda-cont-name">
+                                <div class="name" >  {{scheda.content.titolo}}</div>
 
-                        </ion-col> 
-                    </div>         
-                </ion-row>
-            </template>
-        </ion-grid>
-       
-    </div>
-</ion-content>
+                            </ion-col> 
+                        </div>         
+                    </ion-row>
+                </template>
+            </ion-grid>
+            <div v-else class="text-alert">
+              {{$t('raccolta.alerttext')}}
+            </div>
+        
+        </div>
+    </ion-content>
    
 
 </template>
 
 <script>
    // import common from '@/js/common';
-
     export default ({
        
         computed:{
 
             visited(){
-               const visitedTag=localStorage.getItem("schede_viste").split(',');
-               this.savedtag(visitedTag);
-                const schedemostra=JSON.parse(localStorage.getItem("dataMostra"));
-                const visitedSchede=schedemostra.filter(scheda => visitedTag.includes(scheda.tag));
-                console.log("VISITED ", visitedSchede);
-                let lang= localStorage.getItem("lang");
-                if (lang==null) {
-                    lang=this.$i18n.locale;
-                }
+                let visitedTag=localStorage.getItem("schede_viste");
+                if(visitedTag){
+                    visitedTag=visitedTag.split(',');
+                    this.savedtag(visitedTag);
+                    const schedemostra=JSON.parse(localStorage.getItem("dataMostra"));
+                    const visitedSchede=schedemostra.filter(scheda => visitedTag.includes(scheda.tag));
+                    console.log("VISITED ", visitedSchede);
+                    let lang= localStorage.getItem("lang");
+                    if (lang==null) {
+                        lang=this.$i18n.locale;
+                    }
 
-                visitedSchede.forEach(scheda => {
-                    scheda.content= scheda.content.find(content=> content.lang==lang)
-                    
-                });
-                return visitedSchede;
+                    visitedSchede.forEach(scheda => {
+                        scheda.content= scheda.content.find(content=> content.lang==lang)
+                        
+                    });
+                    return visitedSchede;
+                }else{
+                    return null;
+                }
+               
             }
         },
         methods:{
+            back(){
+                this.$router.replace("/")
+            },
             savedtag(tags){
                 this.visitedTag=tags;
             },
             openScheda(tag, type){
                 if(type=="video"){
-                     this.$router.replace({ path: "/video/" + tag});
+                    this.$router.replace({ path: "/video/" + tag});
 
                 }else{
-                       this.$router.replace({ path: "/audio/" + tag});
+                    this.$router.replace({ path: "/audio/" + tag});
                 }
 
-            }
+            },
+
+           
         }
 
     })
@@ -72,12 +92,20 @@
 
 <style >
 .collection-title{
-    margin-top: 4vh;
+    
+    margin-top: 2vh;
+    margin-left: 5vw;
+    font-size: 18px;
+    color: #008dd1;
 }
 .list-container{
-    height: 82vh;
+    height: 80vh;
     overflow: overlay;
-      margin-top: 4vh;
+      margin-top: 3vh;
+}
+.text-alert{
+    padding: 30px;
+
 }
 
  .thumb-cont{
@@ -96,10 +124,12 @@
  .schede-cont{
    display: flex;
     margin: 0.5em 1em;
-    border-bottom: solid 4px var(--ion-color-secondary-whitened);
-    background-color: #ededed ;
+   /* border-bottom: solid 4px var(--ion-color-secondary-whitened);*/
+    /*background-color: #ededed ;*/
     width: 100%;
     padding: 0.4em 0.5em;
+    border-radius: 4px;
+    box-shadow: 1px 4px 5px 2px #136c974a;
  }
   .scheda-cont-thumb{
    
